d2e59b872a963b040aa6df62115e1f7ba4735eb7
athiyaman-m <athiyamanpro@gmail.com>
2024-03-24 23:06:24 +0530

    UI changes

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+#Imported necesseary modules
 import streamlit as st
 import whisper
 
-st.title("Text To Audio Generation")
+st.title("Audio To Text Generation")
 
+st.text("Step 1 : Click the below browse file and upload your audio file.")
+st.text("Step 2 : After it uploaded, you can play, hear and verify it in the left side bar.")
+st.text("Step 3 : Click 'Transcribe Audio' button.")
+
+#Upload external audio
 audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
 
+#Model loading
 model = whisper.load_model("base")
-st.text("Whisper Model Loaded")
 
+#sidebar
+st.sidebar.header("Play your Audio file")
+st.sidebar.audio(audio_file)
+
+st.subheader("Output : ")
 
 if st.sidebar.button("Transcribe Audio"):
     if audio_file is not None:
@@ -18,6 +29,6 @@ if st.sidebar.button("Transcribe Audio"):
     else:
         st.sidebar.error("Please upload an audio file")
 
-
-st.sidebar.header("Play Original Audio File")
-st.sidebar.audio(audio_file)
+st.sidebar.divider()
+st.sidebar.write("This project was developed by,")
+st.sidebar.write("Athiyaman M")
